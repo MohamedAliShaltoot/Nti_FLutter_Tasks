d1e0cb408cf6d90ae7e293e079660e3a3a32aa8a
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-04-19 18:58:39 +0200

    add app_assets and app_colors

---
+++ lib/core/app_assets.dart
@@ -0,0 +1,8 @@
+abstract class AppAssets {
+  static const String basePath ="assets/images/";
+  static const String logo = "${basePath}password.svg";
+  static const String lock = "${basePath}lock.svg";
+  static const String profile = "${basePath}profile.svg";
+  static const String unlock = "${basePath}unlock.svg";
+
+}
---
+++ lib/core/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors{
+  static const Color scaffoldBackground = Color(0xfff3f5f4);
+  static const Color primary = Color(0xff149954);
+}
