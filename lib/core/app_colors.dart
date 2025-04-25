a7f58d342469d77f9d64b18c1900b3a3a95d08de
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-04-25 23:12:58 +0300

    deleting non-organized files to be organized one in core/utils/ folders

--- lib/core/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract class AppColors{
-  static const Color scaffoldBackground = Color(0xfff3f5f4);
-  static const Color primary = Color(0xff149954);
-}
