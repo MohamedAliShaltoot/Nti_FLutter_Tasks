e5efda2b92fff3b35c20c505f7842910db5b9cb3
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-04-29 18:45:27 +0300

    test on home screen done sucessfully

--- lib/features/home_screen/view/home_tasks_view.dart
+++ lib/features/home_screen/view/home_tasks_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nti_flutter_tasks/features/add_task_screen/view/add_task_view.dart';
 
 import '../../../core/helper/svg_picture_custom.dart';
 import '../../../core/utils/app_assets.dart';
@@ -23,7 +24,14 @@ class HomeTaskContentScreen extends StatelessWidget {
         shape: const CircleBorder(),
         backgroundColor: AppColors.primary,
         onPressed: () {
-          
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return AddTaskView();
+              },
+            ),
+          );
         },
         child: setSvgPicture(
           assetPath: AppAssets.paperPlusIcon,
@@ -32,131 +40,143 @@ class HomeTaskContentScreen extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            buildProfileHeader(context: context),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            children: [
+              buildProfileHeader(context: context),
 
-            SizedBox(height: 15),
-            customTaskContentContainer(width: width, height: height),
+              SizedBox(height: 15),
+              customTaskContentContainer(width: width, height: height),
 
-            SizedBox(height: 15),
-            Align(
-              alignment: AlignmentDirectional.centerStart,
-              child: Padding(
-                padding: const EdgeInsetsDirectional.only(start: 10, end: 235),
-                child: Row(
-                  children: [
-                    Text(
-                      "In Progress",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w300,
-                        color: AppColors.black,
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Container(
-                      alignment: Alignment.center,
-                      width: 20,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        color: AppColors.containerBackgroundColor,
-                        borderRadius: BorderRadius.circular(15),
-                        // shape: BoxShape.circle,
-                      ),
-
-                      child: Text(
-                        "5",
+              SizedBox(height: 15),
+              Align(
+                alignment: AlignmentDirectional.centerStart,
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.only(
+                    start: 10,
+                    end: 235,
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        "In Progress",
                         style: TextStyle(
-                          fontSize: 14,
+                          fontSize: 18,
                           fontWeight: FontWeight.w300,
-                          color: AppColors.primary,
+                          color: AppColors.black,
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(width: 10),
+                      Container(
+                        alignment: Alignment.center,
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                          color: AppColors.containerBackgroundColor,
+                          borderRadius: BorderRadius.circular(15),
+                          // shape: BoxShape.circle,
+                        ),
+
+                        child: Text(
+                          textAlign: TextAlign.center,
+                          "5",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w300,
+                            color: AppColors.primary,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 15),
-            Flexible(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    customCategoryContainer(
-                      title: AppStrings.workTaskTitle,
-                      subTitle: AppStrings.workTaskSubTitle,
+              SizedBox(height: 15),
+              Flexible(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      customCategoryContainer(
+                        title: AppStrings.workTaskTitle,
+                        subTitle: AppStrings.workTaskSubTitle,
 
-                      containerBGC: AppColors.black,
-                      iconBGC: AppColors.primary,
-                      titleBGC: AppColors.white,
-                      subTitleBGC: AppColors.white,
-                      iconPath: AppAssets.workShopIcon,
-                      width: 40,
-                      height: 40,
-                    ),
-                    customCategoryContainer(
-                      title: AppStrings.personalTaskTitle,
-                      subTitle: AppStrings.personalTaskSubTitle,
+                        containerBGC: AppColors.black,
+                        iconBGC: AppColors.primary,
+                        titleBGC: AppColors.white,
+                        subTitleBGC: AppColors.white,
+                        iconPath: AppAssets.workShopIcon,
+                        width: 40,
+                        height: 40,
+                      ),
+                      customCategoryContainer(
+                        title: AppStrings.personalTaskTitle,
+                        subTitle: AppStrings.personalTaskSubTitle,
 
-                      containerBGC: AppColors.lightGreen,
-                      iconBGC: AppColors.primary,
-                      titleBGC: AppColors.gray,
-                      subTitleBGC: AppColors.black,
-                      iconPath: AppAssets.personalGrayIcon,
-                    ),
-                    customCategoryContainer(
-                      title: AppStrings.homeTaskTitle,
-                      subTitle: AppStrings.homeTaskSubTitle,
+                        containerBGC: AppColors.lightGreen,
+                        iconBGC: AppColors.primary,
+                        titleBGC: AppColors.gray,
+                        subTitleBGC: AppColors.black,
+                        iconPath: AppAssets.personalGrayIcon,
+                      ),
+                      customCategoryContainer(
+                        title: AppStrings.homeTaskTitle,
+                        subTitle: AppStrings.homeTaskSubTitle,
 
-                      containerBGC: AppColors.lightPink,
-                      iconBGC: AppColors.pink,
-                      titleBGC: AppColors.gray,
-                      subTitleBGC: AppColors.black,
-                      iconPath: AppAssets.homeIcon,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ],
+                        containerBGC: AppColors.lightPink,
+                        iconBGC: AppColors.pink,
+                        titleBGC: AppColors.gray,
+                        subTitleBGC: AppColors.black,
+                        iconPath: AppAssets.homeIcon,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsetsDirectional.only(start: 10),
-              child: Align(
-                alignment: AlignmentDirectional.centerStart,
-                child: Text(
-                  AppStrings.taskGroupName,
-                  style: TextStyle(fontWeight: FontWeight.w300, fontSize: 16),
+              SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsetsDirectional.only(start: 10),
+                child: Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text(
+                    AppStrings.taskGroupName,
+                    style: TextStyle(fontWeight: FontWeight.w300, fontSize: 18),
+                  ),
                 ),
               ),
-            ),
+              SizedBox(height: 15),
+              customGroupTask(
+                number: 5,
+                title: AppStrings.personalTaskTitle,
+                iconPath: AppAssets.personalGreenIcon,
+                textNumberColor: AppColors.primary,
+                leadingIconBGC: AppColors.lightGreen,
+                traillingIconBGC: AppColors.containerBackgroundColor,
+              ),
+              customGroupTask(
+                number: 3,
+                title: AppStrings.homeTaskTitle,
+                iconPath: AppAssets.homePinkIcon,
+                textNumberColor: AppColors.pink,
+                leadingIconBGC: AppColors.lightPink,
+                traillingIconBGC: AppColors.lightPink,
+              ),
+              customGroupTask(
+                number: 1,
+                title: AppStrings.workTaskTitle,
+                iconPath: AppAssets.workShopIcon,
+                textNumberColor: AppColors.white,
+                leadingIconBGC: AppColors.black,
+                traillingIconBGC: AppColors.black,
 
-            customGroupTask(
-              title: AppStrings.personalTaskTitle,
-              iconPath: AppAssets.personalGreenIcon,
-              textNumberColor: AppColors.primary,
-              leadingIconBGC: AppColors.lightGreen,
-              traillingIconBGC: AppColors.containerBackgroundColor,
-            ),
-            customGroupTask(
-              title: AppStrings.homeTaskTitle,
-              iconPath: AppAssets.homePinkIcon,
-              textNumberColor: AppColors.pink,
-              leadingIconBGC: AppColors.lightPink,
-              traillingIconBGC: AppColors.lightPink,
-            ),
-            customGroupTask(
-              title: AppStrings.workTaskTitle,
-              iconPath: AppAssets.workShopIcon,
-              textNumberColor: AppColors.white,
-              leadingIconBGC: AppColors.black,
-              traillingIconBGC: AppColors.black,
-            ),
-          ],
+
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:nti_flutter_tasks/features/home_screen/view/home_tasks_view.dart
 import 'core/utils/app_colors.dart';
 import 'core/utils/app_strings.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
