341effd078f14d0d7b08fba5b01d76d846878ce4
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-05-13 15:35:59 +0300

    add custom appbar take leading, title, action as a variables to reuse it in add , edit screens

---
+++ lib/core/widgets/custom_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final List<Widget>? actions;
+   final Widget? leading;
+
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    this.actions, this.leading,
+  });
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        title,
+        style: const TextStyle(color: Colors.black),
+      ),
+      centerTitle: true,
+      actions: actions,
+      leading: leading,
+    );
+  }
+}
